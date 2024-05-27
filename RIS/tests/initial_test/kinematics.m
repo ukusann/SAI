6fddbb3a8ad96261cef4dffe8781209cf35ba1fc
Sergio Fernandes <a95710@alunos.uminho.pt>
2024-05-27 18:26:18 +0100

    RIS folder

---
+++ RIS/tests/initial_test/kinematics.m
@@ -0,0 +1,319 @@
+% Functions and algorithms for direct and inverse kinematics of robotic manipulator
+
+classdef kinematics < handle
+    properties
+        kuka_joint_lim_min  % Array with size equal to number of joints
+        kuka_joint_lim_max  % Array with size equal to number of joints 
+        Link                % Array with size equal to number of joints
+    end
+    methods
+        function obj = kinematics(kuka_joint_lim_min, kuka_joint_lim_max, Link)
+            obj.kuka_joint_lim_min = kuka_joint_lim_min;
+            obj.kuka_joint_lim_max = kuka_joint_lim_max;
+            obj.Link = Link;
+        end
+
+        %*********************Direct Kinematics *************************
+        function poseHand = directKinematics(obj, dh_alpha, dh_a, dh_d, dh_theta, theta)
+            %* Compute individual transformation matrices
+            transf_w0 = obj.dhTransfMatrix(dh_alpha(1), dh_a(1), dh_d(1), dh_theta(1));
+            transf_01 = obj.dhTransfMatrix(dh_alpha(2), dh_a(2), dh_d(2), dh_theta(2) + theta(1));
+            transf_12 = obj.dhTransfMatrix(dh_alpha(3), dh_a(3), dh_d(3), dh_theta(3) + theta(2));
+            transf_23 = obj.dhTransfMatrix(dh_alpha(4), dh_a(4), dh_d(4), dh_theta(4) + theta(3));
+            transf_34 = obj.dhTransfMatrix(dh_alpha(5), dh_a(5), dh_d(5), dh_theta(5) + theta(4));
+            transf_45 = obj.dhTransfMatrix(dh_alpha(6), dh_a(6), dh_d(6), dh_theta(6) + theta(5));
+            transf_56 = obj.dhTransfMatrix(dh_alpha(7), dh_a(7), dh_d(7), dh_theta(7) + theta(6));
+            transf_67 = obj.dhTransfMatrix(dh_alpha(8), dh_a(8), dh_d(8), dh_theta(8) + theta(7));
+            %* Compute general transformation matrix T_BE
+            transf_07 = transf_w0 * transf_01 * transf_12 * transf_23 * transf_34 * transf_45 * transf_56 * transf_67;
+
+            %* Cartesian coordinates of Tip {7} with respect to base {0}
+            xh_0=transf_07(1,4);
+            yh_0=transf_07(2,4);
+            zh_0=transf_07(3,4);
+            ph_0=[xh_0, yh_0, zh_0]';
+
+
+            %* Orientation of Tip {7} with respect to base {0}: Roll-Pitch-Yaw
+            %angles:
+            rotation_07 = transf_07(1:3,1:3); 
+            [yaw_x, pitch_y, roll_z] = obj.computeMatrixToRPY(rotation_07);
+            handOrientationDeg = [yaw_x, pitch_y, roll_z]'*180/pi;
+
+            %* Conclusion of Direct kinematics
+            % Tip pose:
+            poseHand = [ph_0; handOrientationDeg];
+        end
+        %*****************************************************************
+
+        %*********************Inverse Kinematics *************************
+        function [error, solutionsNum, joingAngles_sol1, joingAngles_sol2, joingAngles_sol3, joingAngles_sol4] = inverseKinematics(obj, alpha, desPoseHand)
+            joingAngles_sol1 = zeros(1, 7);
+            joingAngles_sol2 = zeros(1, 7);
+            joingAngles_sol3 = zeros(1, 7);
+            joingAngles_sol4 = zeros(1, 7);
+            error = 0;
+            solutionsNum = 4; 
+            
+            % Get hand position and RPY from PoseHand desired
+            desHandPos = desPoseHand(1:3); %todo: Hand position
+            yaw_x   = desPoseHand(4);
+            pitch_y = desPoseHand(5);
+            roll_z  = desPoseHand(6);
+
+            % Create the rotation matrix from RPY 
+            rotation_07 = obj.RPYTransfMatrix(yaw_x, pitch_y, roll_z);
+            
+            % Get transfromation from Base {0} to Hand {7} with rotation matrix and hand position 
+            transf_07 = [rotation_07, desHandPos; zeros(1, 3), 1];
+
+            % Get the z_07 vector
+            z_vec_07 = transf_07(1:3, 3);
+
+            % Calculate desired Wrist Position from Hand Postion, Hand Link and  rotation in Z axis
+            desWristPos = desHandPos - obj.Link(4)*z_vec_07; %todo: Wrist position
+
+            %*Compute Shoulder Position
+            desShoulderPos_0S_h = [0, 0, obj.Link(1), 1]';
+            transf_w0 = [
+                        0 -1 0 0;
+                        1 0 0 0; 
+                        0 0 1 0.4725;
+                        0 0 0 1 
+                        ];
+            desShoulderPos_h = transf_w0*desShoulderPos_0S_h; 
+
+            desShoulderPos = desShoulderPos_h(1:3) %todo: Shoulder position
+            %*****
+
+            %*Compute theta_4 Elbow Angle
+            Lsw = norm(desWristPos - desShoulderPos);
+            argTheta4 = (-obj.Link(2)^2 - obj.Link(3)^2 + Lsw^2)/(2*obj.Link(2)*obj.Link(3));
+            % Check if argument is inside valid limits, i.e., existence condition (sice argument is cos(theta4))
+            if(argTheta4 > 1) || (argTheta4 < -1)
+                disp(['---' newline 'Cannot compute theta4!' newline 'Robot desired position outside workspace!' newline 'Exiting simulation...' newline '---']);
+                error = 1;
+                return;
+            end
+
+            % Since we do not consider the elbow bending backwards, we force only one solution
+            theta4 = -acos(argTheta4); %todo: Theta4
+            
+            if((theta4 > obj.kuka_joint_lim_max(1, 4)) || (theta4 < obj.kuka_joint_lim_min(1, 4)))
+                disp(['---' newline 'Error in theta4!' newline 'Value is outside joint limits!' newline 'Exiting simulation...' newline '---']);
+                error = 1;
+                return;  
+            end
+            %*****
+
+            %*Compute elbow desired position from alpha value
+            sw = desWristPos - desShoulderPos; % direction vectorfrom shoulder to wrist
+            
+            sw_vec = sw / Lsw; % unit vector from shoulder to wrist, ^sw
+            u = [sw_vec(2), -sw_vec(1), 0]'; % n _|_ u
+            u_vec = u / norm(u);
+            
+            v_vec = cross(u_vec , sw_vec); % v = n X u cross product
+            
+            cosbeta = (Lsw^2 + obj.Link(2)^2 - obj.Link(3)^2) / (2 * Lsw * obj.Link(2)); % co-sines law
+            
+            C = desShoulderPos + cosbeta*obj.Link(2) * sw_vec; % center of the circle described by the elbow
+            
+            R = sqrt(1 - cosbeta^2) * obj.Link(2); % radius of the circle described by the elbow
+            
+            % Depends on alpha
+            E = C + R*(cos(alpha)*u_vec + sin(alpha)*v_vec); % desired elbow position
+            
+            desElbowPos = E; %todo: Elbow position
+            %*****
+
+            %* Compute unit vectors of arm links
+            SE_vec = (desElbowPos - desShoulderPos) / obj.Link(2); % unit vector v_SE = (E - S) / norm(E - S)
+            EW_vec = (desWristPos - desElbowPos) / norm(desWristPos - desElbowPos); % unit vector v_EW = (WE) / norm(W - E)
+            SW_vec = sw_vec; % unit vector from Shoulder to wrist
+            CE_vec = (desElbowPos - C) / norm(desElbowPos - C);
+            %*****
+
+            %* Compute elbow's reference frame {4}
+            x_vec_04 = cross(SW_vec, CE_vec); % z4 = SW_vec X CE_vec cross product
+            y_vec_04 = EW_vec;
+            z_vec_04 = cross(y_vec_04, x_vec_04);
+            %*****
+
+            %* Compute theta1, theta2, theta3
+            rotation_04 = [x_vec_04, y_vec_04, z_vec_04];
+            rotation_34 = [
+                        cos(theta4)     -sin(theta4)   0.0;
+                        0.0             0.0            -1;     
+                        sin(theta4)     cos(theta4)    0.0
+                        ];
+            
+            rotation_03 = rotation_04 * rotation_34';
+            
+            theta2 = atan2(sqrt(1 - rotation_03(2,3)^2), -rotation_03(2,3)); %todo: theta2 sol1
+            % Put solution 2 always negative
+            if(theta2 <= 0)
+                theta2_b = theta2;
+                theta2_a = -theta2;
+            else
+                theta2_a = theta2;
+                theta2_b = -theta2;
+            end
+
+            % Check for singularity
+            if(abs(theta2_b) < 0.01)
+                theta2_b = 0;
+                theta1_b = 0;
+                theta3_b = atan2(rotation_03(3,1), rotation_03(3,2));
+                solutionsNum = solutionsNum - 2; 
+                if((theta3_b > obj.kuka_joint_lim_max(1, 3)) || (theta3_b < obj.kuka_joint_lim_min(1, 3)))
+                    disp(['---' newline 'Error in theta3!' newline 'Value is outside joint limits!' newline 'Exiting simulation...' newline '---']);
+                    error = 1;
+                    return; 
+                end
+            else
+                theta1_a = atan2((-rotation_03(3,3) / sin(theta2_a)), (-rotation_03(1,3) / sin(theta2_a))); %todo: theta1 sol1
+                theta3_a = atan2(( rotation_03(2,2) / sin(theta2_a)), ( rotation_03(2,1) / sin(theta2_a))); %todo: theta3 sol1          
+            
+            
+                theta1_b = atan2((-rotation_03(3,3) / sin(theta2_b)), (-rotation_03(1,3) / sin(theta2_b))); %todo: theta1 sol2
+                theta3_b = atan2(( rotation_03(2,2) / sin(theta2_b)), ( rotation_03(2,1) / sin(theta2_b))); %todo: theta3 sol2
+                
+                if((theta2_a > obj.kuka_joint_lim_max(1, 2)) || (theta2_b < obj.kuka_joint_lim_min(1, 2)))
+                    disp(['---' newline 'Error in theta2!' newline 'Value is outside joint limits!' newline 'Exiting simulation...' newline '---']);
+                    error = 1;
+                    return;  
+                    
+                elseif((theta1_a > obj.kuka_joint_lim_max(1, 1)) || (theta1_a < obj.kuka_joint_lim_min(1, 1)) || (theta1_b > obj.kuka_joint_lim_max(1, 1)) || (theta1_b < obj.kuka_joint_lim_min(1, 1)))
+                    disp(['---' newline 'Error in theta1!' newline 'Value is outside joint limits!' newline 'Exiting simulation...' newline '---']);
+                    error = 1;
+                    return; 
+                    
+                elseif((theta3_a > obj.kuka_joint_lim_max(1, 3)) || (theta3_a < obj.kuka_joint_lim_min(1, 3)) || (theta3_b > obj.kuka_joint_lim_max(1, 3)) || (theta3_b < obj.kuka_joint_lim_min(1, 3)))
+                    disp(['---' newline 'Error in theta3!' newline 'Value is outside joint limits!' newline 'Exiting simulation...' newline '---']);
+                    error = 1;
+                    return; 
+                end
+            end
+            %*****
+
+            %* Compute theta4, theta5, theta6
+            rotation_47 = rotation_04' * rotation_07;
+
+            theta6 = atan2(sqrt(1 - rotation_47(2,3)^2), rotation_47(2,3)); %todo: theta6 sol1
+            % Put solution 2 always negative
+            if(theta6 <= 0)
+                theta6_b = theta6;
+                theta6_a = -theta6;
+            else
+                theta6_a = theta6;
+                theta6_b = -theta6;
+            end
+
+            % Check for singularity
+            if(abs(theta6_b) < 0.01)
+                theta6_b = 0;
+                theta5_b = 0;
+                theta7_b = atan2(rotation_47(1,2), rotation_47(3,2));
+                solutionsNum = solutionsNum - 2;
+                if((theta7_b > obj.kuka_joint_lim_max(1, 7)) || (theta7_b < obj.kuka_joint_lim_min(1, 7)))
+                    disp(['---' newline 'Error in theta7!' newline 'Value is outside joint limits!' newline 'Exiting simulation...' newline '---']);
+                    error = 1;
+                    return; 
+                end
+            else
+                theta5_a = atan2(( rotation_47(3,3) / sin(theta6_a)), (-rotation_47(1,3) / sin(theta6_a))); %todo: theta5 sol1
+                theta7_a = atan2((-rotation_47(2,2) / sin(theta6_a)), ( rotation_47(2,1) / sin(theta6_a))); %todo: theta7 sol1
+
+                theta5_b = atan2(( rotation_47(3,3) / sin(theta6_b)), (-rotation_47(1,3) / sin(theta6_b))); %todo: theta5 sol2
+                theta7_b = atan2((-rotation_47(2,2) / sin(theta6_b)), ( rotation_47(2,1) / sin(theta6_b))); %todo: theta7 sol2
+                
+                if((theta6_a > obj.kuka_joint_lim_max(1, 6)) || (theta6_b < obj.kuka_joint_lim_min(1, 6)))
+                    disp(['---' newline 'Error in theta6!' newline 'Value is outside joint limits!' newline 'Exiting simulation...' newline '---']);
+                    error = 1;
+                    return;  
+                
+                elseif((theta5_a > obj.kuka_joint_lim_max(1, 5)) || (theta5_a < obj.kuka_joint_lim_min(1, 5)) || (theta5_b > obj.kuka_joint_lim_max(1, 5)) || (theta5_b < obj.kuka_joint_lim_min(1, 5)))
+                    disp(['---' newline 'Error in theta5!' newline 'Value is outside joint limits!' newline 'Exiting simulation...' newline '---']);
+                    error = 1;
+                    return; 
+                    
+                elseif((theta7_a > obj.kuka_joint_lim_max(1, 7)) || (theta7_a < obj.kuka_joint_lim_min(1, 7)) || (theta7_b > obj.kuka_joint_lim_max(1, 7)) || (theta7_b < obj.kuka_joint_lim_min(1, 7)))
+                    disp(['---' newline 'Error in theta7!' newline 'Value is outside joint limits!' newline 'Exiting simulation...' newline '---']);
+                    error = 1;
+                    return; 
+                end
+            end
+
+            if(solutionsNum == 4)
+                joingAngles_sol1 = [theta1_a, theta2_a, theta3_a, theta4, theta5_b, theta6_b, theta7_b]';
+                joingAngles_sol2 = [theta1_a, theta2_a, theta3_a, theta4, theta5_a, theta6_a, theta7_a]';
+                joingAngles_sol3 = [theta1_b, theta2_b, theta3_b, theta4, theta5_b, theta6_b, theta7_b]';
+                joingAngles_sol4 = [theta1_b, theta2_b, theta3_b, theta4, theta5_a, theta6_a, theta7_a]';
+            
+            elseif(solutionsNum == 2)
+                if(theta2 == 0)
+                    joingAngles_sol3 = [theta1_b, theta2_b, theta3_b, theta4, theta5_b, theta6_b, theta7_b]';
+                    joingAngles_sol4 = [theta1_b, theta2_b, theta3_b, theta4, theta5_a, theta6_a, theta7_a]';
+                else
+                    joingAngles_sol1 = [theta1_a, theta2_a, theta3_a, theta4, theta5_b, theta6_b, theta7_b]';
+                    joingAngles_sol3 = [theta1_b, theta2_b, theta3_b, theta4, theta5_b, theta6_b, theta7_b]';
+                end
+            else
+                solutionsNum = 1; % If theta6 and theta2 == 0, then solutionsNum would be equal to 0, we will restore its value to 1 
+                joingAngles_sol3 = [theta1_b, theta2_b, theta3_b, theta4, theta5_b, theta6_b, theta7_b]';
+            end
+        end
+        %*****************************************************************
+    end
+
+
+    methods (Access = private)
+        function transfMatrix = dhTransfMatrix(~, alpha, a, d, theta)
+            transfMatrix = [
+                    cos(theta)               -sin(theta)               0.0               a;
+                    sin(theta)*cos(alpha)     cos(theta)*cos(alpha)   -sin(alpha)     -sin(alpha)*d;
+                    sin(theta)*sin(alpha)     cos(theta)*sin(alpha)    cos(alpha)      cos(alpha)*d;
+                    0.0                       0.0                      0.0             1.0
+                    ];
+        end
+    
+        function [yaw_x, pitch_y, roll_z] = computeMatrixToRPY(~, rot)
+            psi = atan2(-rot(3,1),sqrt(rot(1,1)^2+rot(2,1)^2));
+            
+            if (abs(psi - pi/2) < 0.01)     % psi == pi/2 % 
+                phi = 0;
+                theta = atan2(rot(1,2),rot(2,2));
+    
+            elseif (abs(psi + pi/2) < 0.01) % psi == -pi/2
+                phi = 0;
+                theta = - atan2(rot(1,2),rot(2,2));
+            else
+                phi = atan2(rot(2,1)/cos(psi),rot(1,1)/cos(psi));
+                theta = atan2(rot(3,2)/cos(psi),rot(3,3)/cos(psi));
+            end
+            if(abs(theta) < 0.01)
+                theta = 0.0;
+            end
+    
+            if(abs(psi) < 0.01)
+                psi = 0.0;
+            end
+            
+            if(abs(phi) < 0.01)
+                phi = 0.0;
+            end
+            yaw_x = theta;
+            pitch_y = psi;
+            roll_z = phi; 
+        end
+    
+        function RPYmatrix = RPYTransfMatrix(~, yaw_x, pitch_y, roll_z)
+            RPYmatrix = [
+                    cos(roll_z)*cos(pitch_y)    -sin(roll_z)*cos(yaw_x)+cos(roll_z)*sin(pitch_y)*sin(yaw_x)      sin(roll_z)*sin(yaw_x)+cos(roll_z)*sin(pitch_y)*cos(yaw_x);
+                    sin(roll_z)*cos(pitch_y)     cos(roll_z)*cos(yaw_x)+sin(roll_z)*sin(pitch_y)*sin(yaw_x)     -cos(roll_z)*sin(yaw_x)+sin(roll_z)*sin(pitch_y)*cos(yaw_x);
+                   -sin(pitch_y)                 cos(pitch_y)*sin(yaw_x)                                         cos(pitch_y)*cos(yaw_x)
+                    ];
+        end 
+    end
+end
---
+++ RIS/tests/initial_test/main_program.m
@@ -0,0 +1,307 @@
+%   main_program.m
+%   Main program for the control of manipulators
+%   Author: Luís Louro, llouro@dei.uminho.pt
+%           Estela Bicho, estela.bicho@dei.uminho.pt
+%   % Copyright (C) 2024
+%   2024/02/19
+%--------------------------------------------------------------------------
+% Before running this script, open the scenario in CoppeliaSim, e.g
+% Do not run simulation!
+%--------------------------------------------------------------------------
+clear
+% need to choose the arm to control
+% robot_name = 'UR10';
+%robot_name = 'Kuka_LBRiisy';
+robot_name = 'LBR_iiwa_14_R820';
+
+% need to choose the gripper/hand
+hand_name = 'RG2';
+
+%Number of targets for each colour of box
+salsageCan  = [1, 2, 3];
+salsageCanPos = zeros(3, 3);
+mushroomCan = [4, 5, 6];
+mushroomCanPos = zeros(3, 3);
+
+%Number of positions in the shelves
+frontShelf1 = [ 1,  2,  3,  4,  5,  6,  7,  8];
+frontShelf1Pos = zeros(8, 3);
+frontShelf2 = [ 9, 10, 11, 12, 13, 14, 15, 16];
+frontShelf2Pos = zeros(8, 3);
+frontShelf3 = [17, 18, 19, 20, 21, 22, 23, 24];
+frontShelf3Pos = zeros(8, 3);
+frontShelf4 = [25, 26, 27, 28, 29, 30, 31, 32];
+frontShelf4Pos = zeros(8, 3);
+frontShelf5 = [33, 34, 35, 36, 37, 38, 39, 40];
+frontShelf5Pos = zeros(8, 3);
+frontShelf6 = [41, 42, 43, 44, 45, 46, 47, 48];
+frontShelf6Pos = zeros(8, 3);
+
+
+% Creation of a communication class object with the simulator
+% Try to connect to simulator
+% Output:
+% sim - pointer to class simulator_interface
+% error_sim = 1 - impossible to connect to simulator
+[sim,error_sim] = simulator_interface();
+if(error_sim==1)
+    return;
+end
+
+%Creation of a communication class object with the manipulator arm
+% Input:
+% sim - pointer to class simulator_interface
+% robot_name - name of arm CoppeliaSim model
+% hand_name - name of hand CoppeliaSim model
+% Output:
+% robot_arm - pointer to class arm_interface
+% error_sim = 1 - impossible to connect to simulator
+[robot_arm,error_man] = arm_interface(sim,robot_name,hand_name);
+if error_man == 1
+    sim.terminate();
+    return;
+end
+
+[error,timestep] = sim.get_simulation_timestep();
+%get time step value (normally dt=50ms)
+if error == 1
+    sim.terminate();
+    return;
+end
+
+[error,nJoints,Links,DistanceHand,MinPositionJoint,MaxPositionJoint] = robot_arm.get_RobotCharacteristics();
+%nJoints - number of arm joints.
+%Links - dimensions of the links between the axes of rotation
+%DistanceHand - distance between the tip of the manipulator and the palm of
+% the hand
+%MinPositionJoint - array with minimum position for joint 1-6
+%MaxPositionJoint - array with maximum position for joint 1-6
+if error == 1
+    sim.terminate();
+    return;
+end
+
+[error,theta] = robot_arm.get_joints();
+%theta - Values for each joint.
+if error == 1
+    sim.terminate();
+    return;
+end
+
+%--------------------------------------------------------------------------
+error = sim.move_conveyorbelt(); %Put the conveyor belt in motion
+if error == 1
+    sim.terminate();
+    return;
+end
+
+error = robot_arm.open_hand(); %Initialize with hand opened
+if error == 1
+    sim.terminate();
+    return;
+end
+%*==================================================
+%*=================Parameters=======================
+%******* Default Variables ******* 
+start = tic;
+m=1;
+stop=0;
+%***********************
+
+
+%*Variables of robotic arm
+% Joint limits
+kuka_joint_lim_min = MinPositionJoint;
+kuka_joint_lim_max = MaxPositionJoint;
+
+% Speed limits
+kuka_vel_lim = pi/180*[85, 85, 100, 75, 130, 135, 135];
+
+% Denavit-Hartenberg modified parameters: alpha(i-1), a(i-1), di, thetai
+dh_alpha = pi/180*[0, -90, 90, 90, -90, -90, 90]';
+dh_a = [0, 0, 0, 0, 0, 0, 0]';
+dh_d = [Links(1), 0, Links(2), 0, Links(3), 0, Links(4)]';
+dh_theta = pi/180*[0, 0, 0, 0, 0, 0, 0]';
+
+dh_alpha_w = 0;
+dh_a_w = 0;
+dh_d_w = 0.4725;
+dh_theta_w = pi/180*90;
+
+%*Creation of kinematics class
+kuka_kinematics = kinematics(kuka_joint_lim_min, kuka_joint_lim_max, Links);
+
+
+
+
+%************** Inverse Kinematics ******************
+alpha = -50*pi/180;
+desPoseHand = [0.2, -0.4, 0.2, 0, 0, 0]';
+tmp = [0.2, 0.4, 0.9, 0, 0, 0]';
+posHand_h = [desPoseHand(1:3); 1];
+transf_w0 = [
+    0 -1 0 0;
+    1 0 0 0; 
+    0 0 1 0.4725;
+    0 0 0 1 
+    ];
+    
+    posHand_ref0_h = transf_w0 * posHand_h; 
+    desPoseHand_ref0 = [posHand_ref0_h(1:3); desPoseHand(4:6)];
+[error, solutionsNum, joingAnglesSol1, joingAnglesSol2, joingAnglesSol3, joingAnglesSol4] = kuka_kinematics.inverseKinematics(alpha, tmp);
+if(error == 1)
+    sim.terminate();
+    return;
+end
+disp(['Inverse Kinematics Number of Solutions: ', num2str(solutionsNum)])
+robot_arm.set_joints(joingAnglesSol4);
+
+%************** Direct Kinematics ******************
+% poseHand = kuka_kinematics.directKinematics([dh_alpha_w; dh_alpha], [dh_a_w; dh_a], [dh_d_w; dh_d], [dh_theta_w; dh_theta], theta)
+dir_flag = 1;
+delay_dir = 0;
+%****************************************************
+
+%****************************************************
+
+%*==================================================
+while stop==0
+    %----------------------------------------------------------------------
+    %% Robot interface
+    % set and get information to/from vrep
+    % avoid do processing in between ensure_all_data and trigger_simulation
+    sim.ensure_all_data();
+
+    % theta - get joint value (rad) for arm
+    [error,theta] = robot_arm.get_joints();
+    if error == 1
+        sim.terminate();
+        return;
+    end
+
+    % armPosition - get robot position
+    [error,armPosition] = robot_arm.get_robot_position();
+    if error == 1
+        sim.terminate();
+        return;
+    end
+
+    % objectPosition - get object position
+    for i = 1:3
+        [error, salsageCanPos(i, :)] = sim.get_object_position(salsageCan(i));
+        if(error == 1)
+            sim.terminate();
+            return;
+        end
+
+        [error, mushroomCanPos(i, :)] = sim.get_object_position(mushroomCan(i));
+        if(error == 1)
+            sim.terminate();
+            return;
+        end
+    end
+
+    % objectPosition - get target position
+    for i = 1:8
+        [error,frontShelf1Pos(i, :)]=sim.get_target_position(frontShelf1(i));        
+        if(error == 1)
+            sim.terminate();
+            return;
+        end
+
+        [error,frontShelf2Pos(i, :)]=sim.get_target_position(frontShelf2(i));        
+        if(error == 1)
+            sim.terminate();
+            return;
+        end
+
+        [error,frontShelf3Pos(i, :)]=sim.get_target_position(frontShelf3(i));        
+        if(error == 1)
+            sim.terminate();
+            return;
+        end
+
+        [error,frontShelf4Pos(i, :)]=sim.get_target_position(frontShelf4(i));        
+        if(error == 1)
+            sim.terminate();
+            return;
+        end
+
+        [error,frontShelf5Pos(i, :)]=sim.get_target_position(frontShelf5(i));        
+        if(error == 1)
+            sim.terminate();
+            return;
+        end
+
+        [error,frontShelf6Pos(i, :)]=sim.get_target_position(frontShelf6(i));        
+        if(error == 1)
+            sim.terminate();
+            return;
+        end
+    end
+
+    %get simulation time
+    [error,sim_time] = sim.get_simulation_time();
+    if error == 1
+        sim.terminate();
+        return;
+    end 
+
+    %trigger simulation step
+    sim.trigger_simulation();
+    %----------------------------------------------------------------------
+    % --- YOUR CODE --- %
+    %Direct Kinematics calculation
+    if(dir_flag == 1)
+        delay_dir = delay_dir + toc(start);
+        if(delay_dir > 10)
+            poseHand = kuka_kinematics.directKinematics([dh_alpha_w; dh_alpha], [dh_a_w; dh_a], [dh_d_w; dh_d], [dh_theta_w; dh_theta], theta)
+            delay_dir = 0;
+            dir_flag = 0;
+        end
+    end
+    %Inverse Kinematics - Send values for joints
+    %Write joints.
+    %armJoints(1)=0*pi/180;
+    %armJoints(2)=90*pi/180;
+    %armJoints(3)=0*pi/180;
+    %armJoints(4)=0*pi/180;
+    %armJoints(5)=0*pi/180;
+    %armJoints(6)=0*pi/180;
+    %armJoints(7)=0*pi/180;
+    %error = robot_arm.set_joints(armJoints) %send value for arm Joints in rad
+    %if error == 1
+    %    sim.terminate();
+    %    return;
+    %end
+
+    %Functions that allows open/close the hand
+    %error = robot_arm.open_hand();     %open
+    %error = robot_arm.close_hand();    %close
+    %if error == 1
+    %    sim.terminate();
+    %    return;
+    %end
+
+    %Function that allows you to put the conveyor belt in motion/stop
+    %error = sim.move_conveyorbelt();     %motion
+    %error = sim.stop_conveyorbelt();     %stop
+    %if error == 1
+    %    sim.terminate();
+    %    return;
+    %end
+
+    m=m+1;
+    %----------------------------------------------------------------------
+    %It allows to guarantee a minimum cycle time of 50ms for the
+    %computation cycle in Matlab
+    time = toc(start);
+    if time<0.05
+        pause(0.05-time);
+    else
+        pause(0.01);
+    end
+    start = tic;
+    %----------------------------------------------------------------------
+end
+error = sim.terminate();
