f881c0af28bdf700c9f316f0913445c475d17d5d
Sergio Fernandes <a95710@alunos.uminho.pt>
2024-06-11 00:25:11 +0100

    Update with final GUI

---
+++ tests/robotic_arm/testPlaceShelfRight/states.m
@@ -0,0 +1,7 @@
+% Enumeration class for the states
+
+classdef states
+    enumeration
+        Idle, GoToTarget, InitParking, MoveArmEndEffector, PickBox, PlaceBox, MoveArmTransp, ExitParking, GoToDefPos
+    end
+end
